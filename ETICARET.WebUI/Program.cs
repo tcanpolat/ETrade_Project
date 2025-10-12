daa727cb4e440e347f24cd4ad66f9d5aca27f61a
tcanpolat <thsn_cnplt@windowslive.com>
2025-10-12 13:01:51 +0300

    identity seed and datacontext

--- ETICARET.WebUI/Program.cs
+++ ETICARET.WebUI/Program.cs
@@ -1,3 +1,7 @@
+using ETICARET.WebUI.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace ETICARET.WebUI
 {
     public class Program
@@ -8,6 +12,42 @@ namespace ETICARET.WebUI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"))
+            );
+
+            // Identity Servisleri
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                            .AddDefaultTokenProviders();
+            
+            // Seed Identity
+            var userManager = builder.Services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
+            var roleManager = builder.Services.BuildServiceProvider().GetService<RoleManager<IdentityRole>>();
+
+            // Identity Configuration
+            builder.Services.Configure<IdentityOptions>(
+                options =>
+                {
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 6;
+
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.AllowedForNewUsers = true;
+
+                    options.User.RequireUniqueEmail = true;
+                    options.SignIn.RequireConfirmedEmail = true;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+                }     
+                
+           );
 
             var app = builder.Build();
 
---
+++ ETICARET.WebUI/wwwroot/js/CommentsImages.js
@@ -0,0 +1,100 @@
+﻿CommentBodyId = "#comment"
+var productId = -1
+function imageBox(smallImg) {
+    var fullImg = document.getElementById("image-box")
+    fullImg.src = smallImg.src
+}
+
+$(document).ready(function () {
+    var url = $("#comment").data("url")
+    $("#comment").load(url)
+    productId = $("#comment").data("product-id")
+    $(CommentBodyId).load("/Comment/ShowProductComments?id="+productId)
+})
+
+function doComment(btn, e, commentId, spanId) {
+    var button = $(btn)
+    var mode = button.data("edit-mode")
+    var editableContent = $("#comment_text_" + commentId)
+
+    if (e == 'new_clicked') {
+        var txt = $("#new_comment_text").val()
+
+        $.ajax({
+            method: "POST",
+            url: '/Comment/Create',
+            data: { 'text': txt, 'productId': productId }
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+            }
+            else {
+                alert("Yorum yapılırken bir hata oluştu!")
+            }
+        }).fail(function (error) {
+            alert("Sunucuda bir hata oluştu!")
+        })
+    }
+    else if (e == 'delete_clicked') {
+        var dialog_res = confirm("Yorum Silinsin mi?")
+
+        if (!dialog_res) return false
+
+        $.ajax({
+            method: "POST",
+            url: '/Comment/Delete?id=' + commentId,
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+            }
+            else {
+                alert("Yorum Silinemedi!")
+            }
+        }).fail(function (error) {
+            alert("Sunucuda bir hata oluştu!")
+        })
+    }
+    else if(e == 'edit_clicked') {
+        if (!mode) {
+            button.data("edit-mode", true)
+            button.removeClass("btn-warning")
+            button.addClass("btn-success")
+            var btnSpan = button.find("span")
+            btnSpan.removeClass("fa-edit")
+            btnSpan.addClass("fa-check")
+            editableContent.addClass("editable-content")
+            editableContent.addClass("editableComment")
+            $(spanId).attr("contenteditable",true)
+        }
+        else {
+            button.data("edit-mode", false)
+            button.removeClass("btn-success")
+            button.addClass("btn-warning")
+            var btnSpan = button.find("span")
+            btnSpan.removeClass("fa-check")
+            btnSpan.addClass("fa-edit")
+            editableContent.removeClass("editable-content")
+            editableContent.removeClass("editableComment")
+            $(spanId).attr("contenteditable", true)
+
+            var txt = $(spanId).text().trim(' ')
+
+            $.ajax({
+                method: "POST",
+                url: '/Comment/Edit',
+                data: { 'text': txt, 'id': commentId }
+            }).done(function (data) {
+                if (data.result) {
+                    $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+                }
+                else {
+                    alert("Yorum Güncellenemedi!")
+                }
+            }).fail(function (error) {
+                alert("Sunucuda bir hata oluştu!")
+            })
+
+
+        }
+    }
+}
